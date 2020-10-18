8f5310b485a7ec58df10fcbfe29757579c3b37ac
Adri Forczek <forczekadri@gmail.com>
2020-10-18 10:50:48 +0100

    fix: collects methodCallExpr names to avoid duplicating expressions with different parameters

--- src/visitors/MethodAndRemoteMethodCountVisitor.java
+++ src/visitors/MethodAndRemoteMethodCountVisitor.java
@@ -2,7 +2,6 @@ package visitors;
 
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 
@@ -14,25 +13,20 @@ public class MethodAndRemoteMethodCountVisitor extends GenericVisitorAdapter<Int
     @Override
     public Integer visit(ClassOrInterfaceDeclaration n, Void arg) {
         // returns number of methods and methodCallExprs within those methods
-        return n.getMethods().size() + super.visit(n, arg);
-    }
-
-    @Override
-    public Integer visit(MethodDeclaration n, Void arg) {
-        Set<MethodCallExpr> methodCallExpressions = new HashSet<>();
-        return traverseTree(n, methodCallExpressions).size();
+        return n.getMethods().size() +
+                collectMethodCallExpr(n, new HashSet<>()).size();
     }
 
     // traverses method tree and counts all method calls within that method
-    private Set<MethodCallExpr> traverseTree(Node node, Set<MethodCallExpr> methodCallExpressions) {
+    private Set<String> collectMethodCallExpr(Node node, Set<String> methodCallExpressions) {
         if(node.getChildNodes().isEmpty()) {
             return methodCallExpressions;
         } else {
             for(Node child : node.getChildNodes()){
                 if(child instanceof MethodCallExpr) {
-                    methodCallExpressions.add((MethodCallExpr) child);
+                    methodCallExpressions.add(((MethodCallExpr) child).getNameAsString());
                 }
-                methodCallExpressions.addAll(traverseTree(child, methodCallExpressions));
+                methodCallExpressions.addAll(collectMethodCallExpr(child, methodCallExpressions));
             }
         }
         return methodCallExpressions;
