447791ff40aa47f83556f08e5029bf3f83865268
Adri Forczek <forczekadri@gmail.com>
2020-10-19 09:57:54 +0100

    chore: removes unused getName method

--- src/com/metricsassignment/metrics/RFCMetric.java
+++ src/com/metricsassignment/metrics/RFCMetric.java
@@ -3,17 +3,14 @@ package com.metricsassignment.metrics;
 import com.github.javaparser.ast.CompilationUnit;
 import com.metricsassignment.visitors.MethodAndRemoteMethodCountVisitor;
 
-// the number of methods in a class
-// plus the number of remote methods called by methods in the class
+/**
+ * the number of methods in a class plus the number of remote methods called by methods in the class
+ */
 public class RFCMetric implements Metric {
 
     @Override
     public double calculate(CompilationUnit compilationUnit) {
         MethodAndRemoteMethodCountVisitor marm = new MethodAndRemoteMethodCountVisitor();
         return marm.visit(compilationUnit, null);
-    }
-
-    public String getName(){
-        return "RFC Metric";
     }
 }
